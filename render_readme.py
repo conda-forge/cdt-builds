201ba2f24e59faec0fe0cad368be3bb91d1d1209
Matthew R Becker <beckermr@users.noreply.github.com>
2020-07-19 09:36:10 -0500

    ENH add CDT builds to the readme (#15)
    * ENH add CDT builds to the readme
    
    * not this one
    
    * not this one
    
    * not this one

---
+++ render_readme.py
@@ -0,0 +1,33 @@
+import os
+import glob
+
+from jinja2 import Template
+
+from cdt_config import (
+    LEGACY_CDT_PATH,
+    LEGACY_CUSTOM_CDT_PATH,
+    CDT_PATH,
+    CUSTOM_CDT_PATH,
+)
+
+
+def render_readme():
+    recipes = set(
+        glob.glob(CDT_PATH + "/*")
+        + glob.glob(CUSTOM_CDT_PATH + "/*")
+        + glob.glob(LEGACY_CDT_PATH + "/*")
+        + glob.glob(LEGACY_CUSTOM_CDT_PATH + "/*")
+    )
+    recipes = sorted(
+        os.path.basename(r) for r in recipes if not r.endswith("README.md")
+    )
+
+    with open("README.md.tmpl", "r") as fp:
+        tmpl = Template(fp.read())
+
+    with open("README.md", "w") as fp:
+        fp.write(tmpl.render(cdts=recipes))
+
+
+if __name__ == '__main__':
+    render_readme()
