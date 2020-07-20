b16bb2bb5ac34dac0fa5746bb3dd4c0dbe48c75b
Matthew R Becker <becker.mr@gmail.com>
2020-07-20 08:21:46 -0500

    ENH try this

--- legacy_cdts/pango-cos7-aarch64/build.sh
+++ legacy_cdts/pango-cos7-aarch64/build.sh
@@ -21,6 +21,6 @@ popd
 
 pushd ${SYSROOT_DIR}/usr/bin
 rm -f pango-querymodules-64
-echo "/usr/bin/env bash\n exit 0" > pango-querymodules-64
+echo -e '#!/usr/bin/env bash\n exit 0' > pango-querymodules-64
 chmod u+x pango-querymodules-64
 popd
