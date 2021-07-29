b255f5a759de7a95a50b4139f3bfb4df3af536f0
Matthew R. Becker <becker.mr@gmail.com>
2021-07-28 20:19:11 -0500

    REF upload on the fly

--- build_cdt_recipes.py
+++ build_cdt_recipes.py
@@ -164,7 +164,28 @@ def _build_cdt(cdt_meta_node, no_temp=False):
         if c.returncode == 0:
             break
 
-    return c
+    if (
+        os.environ.get("IS_FORK", "True") == "False"
+        and "ANACONDA_TOKEN" in os.environ
+        and os.environ.get("BUILD_SOURCEBRANCHNAME", None) == "master"
+        and c.returncode == 0
+    ):
+        cdt_slug = os.path.basename(cdt_meta_node["recipe_path"])
+        cdt_file = glob.glob(
+            os.path.expandvars("${HOME}/miniforge3/conda-bld/*/%s.tar.bz2" % cdt_slug)
+        )
+        assert len(cdt_file) == 0
+        c_up = subprocess.run(
+            "anaconda --token ${ANACONDA_TOKEN} upload --skip-existing %s" % cdt_file,
+            shell=True,
+            text=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+        )
+    else:
+        c_up = None
+
+    return c, c_up
 
 
 def _build_all_cdts(cdt_path, custom_cdt_path, dist_arch_slug):
@@ -219,14 +240,26 @@ def _build_all_cdts(cdt_path, custom_cdt_path, dist_arch_slug):
 
                 for fut in as_completed(futures):
                     node = futures[fut]
-                    c = fut.result()
+                    c, c_up = fut.result()
                     build_logs += (
                         "\n\n"
                         + LINE_SEP
                         + "\n"
                         + c.stdout
                     )
-                    if c.returncode == 0:
+                    if c_up is not None:
+                        build_logs += (
+                            "\n\n"
+                            + LINE_SEP
+                            + "\n"
+                            + c_up.stdout
+                        )
+
+                    if (
+                        c.returncode == 0
+                        and c_up is not None
+                        and c_up.returncode == 0
+                    ):
                         pbar.write("built %s" % node, file=sys.stderr)
                         sys.stderr.flush()
                         built.add(node)
