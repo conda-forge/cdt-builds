8c4ba017bc46008dc6fdacd5c9737abcb311b595
H. Vetinari <h.vetinari@gmx.com>
2024-10-26 11:50:36 +1100

    move folder_to_package to shared location

--- cdt_config.py
+++ cdt_config.py
@@ -1,2 +1,15 @@
 CDT_PATH = "cdts"
 CUSTOM_CDT_PATH = "custom_cdts"
+
+
+def folder_to_package(folder):
+    # our folders are distinguished by distro, but packages aren't (after alma8)
+    if "centos7" in folder:
+        idx = folder.index("centos7")
+        pkg = folder[:idx] + "cos7" + folder[idx + 7:]
+    elif "alma" in folder:
+        idx = folder.index("alma")  # followed by 8 or 9
+        pkg = folder[:idx] + "conda" + folder[idx + 5:]
+    else:
+        raise ValueError(f"unexpected name {folder}")
+    return pkg
--- render_readme.py
+++ render_readme.py
@@ -6,20 +6,8 @@ from jinja2 import Template
 from cdt_config import (
     CDT_PATH,
     CUSTOM_CDT_PATH,
+    folder_to_package,
 )
-
-
-def folder_to_package(folder):
-    # our folders are distinguished by distro, but packages aren't (after alma8)
-    if "centos7" in folder:
-        idx = folder.index("centos7")
-        pkg = folder[:idx] + "cos7" + folder[idx + 7:]
-    elif "alma" in folder:
-        idx = folder.index("alma")  # followed by 8 or 9
-        pkg = folder[:idx] + "conda" + folder[idx + 5:]
-    else:
-        raise ValueError(f"unexpected name {folder}")
-    return pkg
 
 
 def render_readme():
